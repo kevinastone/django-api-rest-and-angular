56de4e782a056c5099479cc2f6470962230bb0b5
John Franey <johnfraney@gmail.com>
2015-04-22 11:13:04 -0300

    Update permissions.py
    Fix typo

--- example/api/permissions.py
+++ example/api/permissions.py
@@ -11,7 +11,7 @@ class SafeMethodsOnlyPermission(permissions.BasePermission):
 
 
 class PostAuthorCanEditPermission(SafeMethodsOnlyPermission):
-    """Allow everyone to list or view, but only the other can modify existing instances"""
+    """Allow everyone to list or view, but only the author can modify existing instances"""
     def has_object_permission(self, request, view, obj=None):
         if obj is None:
             # Either a list or a create, so no author
