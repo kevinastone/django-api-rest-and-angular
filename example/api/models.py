7b081b8dc98df186f458be28c4134744b0c07cc9
Kevin Stone <kevinastone@gmail.com>
2018-03-03 09:32:55 -0800

    Add mandatory on_delete handler in model FKs

--- example/api/models.py
+++ example/api/models.py
@@ -8,11 +8,11 @@ class User(AbstractUser):
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=255)
     body = models.TextField(blank=True, null=True)
 
 
 class Photo(models.Model):
-    post = models.ForeignKey(Post, related_name='photos')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='photos')
     image = models.ImageField(upload_to="%Y/%m/%d")
